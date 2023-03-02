1050fb4affbb67787788c5b755ce1d11aab561c2
Hunt092 <hunters.hunt92@gmail.com>
2023-03-03 00:34:51 +0530

    integration with frontend

--- static/js/demo/chart-pie-demo.js
+++ static/js/demo/chart-pie-demo.js
@@ -3,36 +3,59 @@
 	'-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = "#858796";
 
-// Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-	type: "doughnut",
-	data: {
-		labels: ["Vendor 1", "Vendor 2", "Sales"],
-		datasets: [
-			{
-				data: [55, 30, 15],
-				backgroundColor: ["#4e73df", "#1cc88a", "#36b9cc"],
-				hoverBackgroundColor: ["#2e59d9", "#17a673", "#2c9faf"],
-				hoverBorderColor: "rgba(234, 236, 244, 1)",
+function getRandomColor() {
+	var letters = "0123456789ABCDEF";
+	var color = "#";
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+fetch("/vendor_total")
+	.then((response) => response.json())
+	.then((data) => {
+		// Do something with the data
+		BackgroundColors = [];
+		data.vendor_name.forEach((vendor) => {
+			BackgroundColors.push(getRandomColor());
+		});
+		var ctx = document.getElementById("myPieChart");
+		var myPieChart = new Chart(ctx, {
+			type: "doughnut",
+			data: {
+				labels: data.vendor_name,
+				datasets: [
+					{
+						data: data.total_amount,
+						backgroundColor: BackgroundColors,
+						hoverBackgroundColor: ["#2e59d9", "#17a673", "#2c9faf"],
+						hoverBorderColor: "rgba(234, 236, 244, 1)",
+					},
+				],
 			},
-		],
-	},
-	options: {
-		maintainAspectRatio: false,
-		tooltips: {
-			backgroundColor: "rgb(255,255,255)",
-			bodyFontColor: "#858796",
-			borderColor: "#dddfeb",
-			borderWidth: 1,
-			xPadding: 15,
-			yPadding: 15,
-			displayColors: false,
-			caretPadding: 10,
-		},
-		legend: {
-			display: false,
-		},
-		cutoutPercentage: 80,
-	},
-});
+			options: {
+				maintainAspectRatio: false,
+				tooltips: {
+					backgroundColor: "rgb(255,255,255)",
+					bodyFontColor: "#858796",
+					borderColor: "#dddfeb",
+					borderWidth: 1,
+					xPadding: 15,
+					yPadding: 15,
+					displayColors: false,
+					caretPadding: 10,
+				},
+				legend: {
+					display: false,
+				},
+				cutoutPercentage: 80,
+			},
+		});
+	})
+	.catch((error) => {
+		// Handle any errors
+		console.error(error);
+	});
+
+// Pie Chart Example
